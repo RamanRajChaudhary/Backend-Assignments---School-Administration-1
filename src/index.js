de7743a520d891b75f40dd82271235442d52f580
Ankita Nigam <ankitanigam51@gmail.com>
2020-12-20 14:41:41 +0530

    check

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 const port = 8080
+const studentArray = require('./InitialData');
+
+let localStudentArray = [...studentArray];
+let maxId = localStudentArray.length;
+
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
@@ -9,6 +14,79 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
+
+app.get("/api/student", (req,res) => {
+    res.send(localStudentArray);
+})
+
+app.get("/api/student/:id", (req,res) => {
+    const idToSearch = req.params.id;
+    const matched = localStudentArray.filter(
+    (student) => student.id === Number(idToSearch)
+    );
+    if(matched.length == 0){
+        res.sendStatus(404);
+    }else{
+     res.send(matched[0]);
+    }
+});
+
+const isNulllOrUndefined = (vol) => vol === null || vol === undefined;
+
+app.post("/api/student", (req,res) => {
+    const newStudent = req.body;
+    const {name, currentClass, division } = newStudent;
+    if(isNulllOrUndefined(name) || isNulllOrUndefined(currentClass) || isNulllOrUndefined(division)){
+        res.sendStatus(400);
+    }else {
+     const newId = maxId + 1;
+     maxId = newId;
+     newStudent.id = newId;
+     localStudentArray.push(newStudent);
+     res.send({ id: newId});
+    }
+});
+
+app.put("/api/student/:id", (req,res) => {
+    const idToSearch = req.params.id;
+    const update = req.body;
+    const {name, currentClass, division } = newStudent;
+    const matchedIdx = localStudentArray.findIndex(
+    (student) => student.id === Number(idToSearch)
+    );
+    if(matchedIdx === -1){
+        res.sendStatus(400);
+    } else{
+     if(!isNulllOrUndefined(name)) {
+         localStudentArray[matchedIdx].name = name;
+         res.sendStatus(200);
+     }
+     else if(!isNulllOrUndefined(currentClass)) {
+        localStudentArray[matchedIdx].currentClass = currentClass;
+        res.sendStatus(200);
+    }
+    else if(!isNulllOrUndefined(division)) {
+        localStudentArray[matchedIdx].division = division;
+        res.sendStatus(200);
+    }
+    else {
+        res.sendStatus(400);
+    }
+    }
+});
+
+app.delete("/api/student/:id", (req,res) => {
+    const idToSearch = req.params.id;
+    const matchedIdx = localStudentArray.findIndex(
+    (student) => student.id === Number(idToSearch)
+    );
+    if(matchedIdx === -1){
+        res.sendStatus(404);
+    } else{
+    localStudentArray = localStudentArray.splice(matchedIdx , 1);
+    res.sendStatus(200);
+    }
+});
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
